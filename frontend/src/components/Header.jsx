72e1928f6ebb9a41689e4f27bd2b10b2cfa19cf0
adamyi515 <adamyi515@gmail.com>
2023-08-21 13:46:45 -0500

    install packages: react-bootstrap bootstrap and react-icons

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { FAShoppingCart, FaUser } from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar>
+            
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
