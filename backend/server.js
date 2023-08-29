e7802fbae59beff872500f34d22563cd38595fe6
adamyi515 <adamyi515@gmail.com>
2023-08-29 15:35:57 -0500

    Install dotenv package and use environment variable.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
 
 import products from './data/products.js';
 
@@ -12,12 +14,18 @@ app.get('/products', (req, res) => {
     res.json(products);
 });
 
+app.get('/products/:id', (req, res) => {
+    const getProduct = products.find(product => product._id === req.params.id);
+    res.json(getProduct);
+})
+
+app.delete('/products/:id', (req, res) => {
+    const getProductToDelete = products.find(product => product._id === req.params.id);
+    res.json(getProductToDelete);
+})
 
 
-
-
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 })
